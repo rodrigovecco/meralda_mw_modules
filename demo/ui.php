ebc523f6dd5779b9eddc0c3cb450088d9d479bb3
Rodrigo Vecco Haddad <rodrigo@novoingenios.com>
2025-05-24 17:57:51 -0500

    Demo

--- demo/ui.php
+++ demo/ui.php
@@ -1,10 +1,11 @@
 <?php
-class mwmod_mw_demo_ui extends mwmod_mw_ui_base_basesubui{
+class mwmod_mw_demo_ui extends mwmod_mw_ui_base_basesubuia{
 	function __construct($cod,$parent){
 		$this->init_as_main_or_sub($cod,$parent);
 		$this->set_lngmsgsmancod("demo");
 		$this->set_def_title($this->lng_get_msg_txt("demo","Demo"));
 		$this->sucods="inputs,icons,extmods";
+		$this->mnuIconClass="meralda-icon meralda-icon-cat";
 
 		
 	}
@@ -65,17 +66,21 @@ class mwmod_mw_demo_ui extends mwmod_mw_ui_base_basesubui{
 		$this->prepare_mnu_item($mnuitem);
 		$mnu->add_item_by_item($mnuitem);
 	}
+	/*
 	function prepare_mnu_item($item){
-		$item->addInnerHTML_icon("glyphicon glyphicon-star");
+		$item->addInnerHTML_icon("meralda-icon meralda-icon-cat");
 	}
+		*/
 	
 	function do_exec_no_sub_interface(){
 	}
+	/*
 	function do_exec_page_in(){
 		echo "<p>This is a Demo UI</p>";
 
 		
 	}
+		*/
 	function is_allowed(){
 		return $this->allow("admin");
 	}
