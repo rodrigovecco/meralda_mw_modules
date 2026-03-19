f2332480439c3053ae4100e27fbe9df717af8d1b
Rodrigo Vecco Haddad <rodrigo@novoingenios.com>
2026-03-19 01:45:18 -0500

    feat: Implement form submission management with IP-based rate limiting and admin UI

--- bruteforce/ui/main.php
+++ bruteforce/ui/main.php
@@ -11,6 +11,41 @@ class mwmod_mw_bruteforce_ui_main extends mwmod_mw_ui_base_basesubuia{
 	function do_exec_page_in(){
 		$man=$this->mainap->get_submanager("bruteforce");
 
+
+
+		$MainContainer=$this->get_ui_dom_elem_container();
+		$container=$MainContainer;
+		if($this->mainPanelEnabled){
+			
+			if($mainpanel=$this->createMainPanel()){
+				$MainContainer->add_cont($mainpanel);
+				$container=$mainpanel->panel_body->add_cont_elem();
+			}
+		
+		}
+		
+		$sbucontainer=$container->add_cont_elem();
+		$sbucontainer1=$sbucontainer->add_cont_elem();
+		$e=$sbucontainer1->add_cont_elem();
+		$e->addClass("card mb-2");
+		$ee=$e->add_cont_elem();
+		$ee->addClass("card-body");
+		$ee->addCont($this->lng_get_msg_txt("yourIP","Tu IP").": ".$man->getCurrentIP());
+		//echo "<div class='card'><div class='card-body'>";
+		if($subs=$this->get_subinterfaces_by_code($this->sucods,true)){
+			$listcontainer=$sbucontainer1->add_cont_elem();
+			$listcontainer->addClass("list-group");
+			//echo "<div class='list-group'>";
+			foreach($subs as $su){
+				$listcontainer->add_cont("<a href='".$su->get_url()."' class='list-group-item list-group-item-action'>".$su->get_mnu_lbl()."</a>");	
+			}
+			//echo "</div>";
+			
+		}
+		echo $MainContainer->get_as_html();
+
+
+		/*
 		echo "<div class='card'><div class='card-body'>";
 		echo "<div>".$this->lng_get_msg_txt("yourIP","Tu IP").": ".$man->getCurrentIP()."</div>";
 		if($subs=$this->get_subinterfaces_by_code($this->sucods,true)){
@@ -22,6 +57,7 @@ class mwmod_mw_bruteforce_ui_main extends mwmod_mw_ui_base_basesubuia{
 			
 		}
 		echo "</div></div>";
+		*/
 		
 	}
 	function _do_create_subinterface_child_activity($cod){
